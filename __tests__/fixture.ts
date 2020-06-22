c6df46bdc9bf160fd279923562211f4198f9c9cc
jaeyoung.choi <lactrious@gmail.com>
2020-06-22 22:38:14 +0900

    Tests on both of simple and auth redis

--- __tests__/fixture.ts
+++ __tests__/fixture.ts
@@ -1,17 +1,72 @@
 import connect, { IRedisConnection } from "../src/connection";
 
-const redisHost = `localhost`;
-const redisPassword = ``;
+import { GenericContainer } from "testcontainers";
+
+async function prepareRedis() {
+  const password = `1234`;
+  const [simpleContainer, authContainer] = await Promise.all([
+    new GenericContainer("redis").withExposedPorts(6379).start(),
+    new GenericContainer("redis")
+      .withExposedPorts(6379)
+      .withCmd(["redis-server", "--requirepass", password])
+      .start(),
+  ]);
+
+  async function stopAllContainers() {
+    await Promise.all([simpleContainer.stop(), authContainer.stop()]);
+  }
+
+  return {
+    simple: {
+      host: simpleContainer.getContainerIpAddress(),
+      port: simpleContainer.getMappedPort(6379),
+    },
+    auth: {
+      host: authContainer.getContainerIpAddress(),
+      port: authContainer.getMappedPort(6379),
+      password,
+    },
+    stopAllContainers,
+  };
+}
+
+const redis = prepareRedis();
+
+afterAll(async () => {
+  const { stopAllContainers: stopContainer } = await redis;
+  await stopContainer();
+});
 
 export default function fixture(
   testName: string,
   connectionWork: (connection: IRedisConnection) => Promise<void>
 ) {
-  test(testName, async () => {
-    const connection = connect({
-      host: redisHost,
-      password: redisPassword,
-    });
+  test(testName + "-on-simple-container", async () => {
+    const {
+      simple: { host, port },
+    } = await redis;
+    const connection = connect({ host, port });
+    try {
+      await connectionWork(connection);
+    } catch (error) {
+      console.error("Uncaught error", error);
+      throw error;
+    } finally {
+      // Clear all entries after test.
+      await connection.socket.send({
+        message: "FLUSHALL\r\n",
+        fulfill: "+OK\r\n".length,
+        timeoutMillis: 1000,
+      });
+      connection.socket.disconnect();
+    }
+  });
+
+  test(testName + "-on-auth-container", async () => {
+    const {
+      auth: { host, port, password },
+    } = await redis;
+    const connection = connect({ host, port, password });
     try {
       await connectionWork(connection);
     } catch (error) {
